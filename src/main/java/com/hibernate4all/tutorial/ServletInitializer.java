325105b073d481b94654a6c4a48d52ec68f8b0c4
ToureMoustapha <moustapha.toure@etu.univ-amu.fr>
2023-12-28 16:41:44 +0100

    fix type

--- src/main/java/com/hibernate4all/tutorial/ServletInitializer.java
+++ src/main/java/com/hibernate4all/tutorial/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.hibernate4all.tutoriel;
+package com.hibernate4all.tutorial;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
