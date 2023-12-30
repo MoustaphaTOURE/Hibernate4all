f5382729154c37482911c3164e7811891b080b80
ToureMoustapha <moustapha.toure@etu.univ-amu.fr>
2023-12-30 23:39:11 +0100

    useless code

--- src/test/java/com/hibernate4all/tutorial/config/PersistenceConfig.java
+++ src/test/java/com/hibernate4all/tutorial/config/PersistenceConfig.java
@@ -52,7 +52,6 @@ public class PersistenceConfig {
 	private Properties additionalProperties() {
 		final Properties properties = new Properties();
 		properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 		return properties;
 	}
 }
